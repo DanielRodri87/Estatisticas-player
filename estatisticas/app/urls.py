89097048f061f615a5efd596a86444d223a43043
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-01-07 15:03:37 -0300

    att

--- estatisticas/app/urls.py
+++ estatisticas/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urrpatterns = [
+urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote', views.detail, name='vote'),
 ]
